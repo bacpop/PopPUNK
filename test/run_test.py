4807a37e5875a191e208b439915272e0c15287cb
John Lees <lees.john6@gmail.com>
2018-06-04 17:29:53 -0400

    Add mash install to travis testing

--- test/run_test.py
+++ test/run_test.py
@@ -8,25 +8,29 @@ import os
 import sys
 import shutil
 
+mash_exec = 'mash'
+if len(sys.argv) > 1:
+    mash_exec = sys.argv[1]
+
 if not os.path.isfile("12754_4#89.contigs_velvet.fa"):
     sys.stderr.write("Extracting example dataset\n")
     subprocess.run("tar xf example_set.tar.bz2", shell=True, check=True)
 
 #easy run
 sys.stderr.write("Running database creation + BGMM model fit (--easy-run)\n")
-subprocess.run("python ../poppunk-runner.py --easy-run --r-files references.txt --min-k 13 --k-step 3 --output example_db --full-db --K 4", shell=True, check=True)
+subprocess.run("python ../poppunk-runner.py --easy-run --r-files references.txt --min-k 13 --k-step 3 --output example_db --full-db --K 4 --mash" + mash_exec, shell=True, check=True)
 
 #fit dbscan
 sys.stderr.write("Running HDBSCAN model fit (--fit-model)\n")
-subprocess.run("python ../poppunk-runner.py --fit-model --distances example_db/example_db.dists --ref-db example_db --output example_db --dbscan --microreact --cytoscape", shell=True, check=True)
+subprocess.run("python ../poppunk-runner.py --fit-model --distances example_db/example_db.dists --ref-db example_db --output example_db --dbscan --microreact --cytoscape --mash " + mash_exec, shell=True, check=True)
 
 #refine model
 sys.stderr.write("Running model refinement (--refine-model)\n")
-subprocess.run("python ../poppunk-runner.py --refine-model --distances example_db/example_db.dists --ref-db example_db --output example_refine --neg-shift 0.8 --full-db", shell=True, check=True)
+subprocess.run("python ../poppunk-runner.py --refine-model --distances example_db/example_db.dists --ref-db example_db --output example_refine --neg-shift 0.8 --full-db --mash " + mash_exec, shell=True, check=True)
 
 #assign query
 sys.stderr.write("Running query assignment (--assign-query)\n")
-subprocess.run("python ../poppunk-runner.py --assign-query --q-files references.txt --ref-db example_db --output example_query", shell=True, check=True)
+subprocess.run("python ../poppunk-runner.py --assign-query --q-files references.txt --ref-db example_db --output example_query --mash " + mash_exec, shell=True, check=True)
 
 sys.stderr.write("Tests completed\n")
 
