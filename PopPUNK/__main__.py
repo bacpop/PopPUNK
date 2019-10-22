66471d13b343eab0ec46f293b2897e219d7a332b
nickjcroucher <n.croucher@imperial.ac.uk>
2019-10-22 11:41:44 +0100

    Permit using external clustering in generate-viz mode

--- PopPUNK/__main__.py
+++ PopPUNK/__main__.py
@@ -434,7 +434,11 @@ def main():
 
             # Read in network and cluster assignment
             genomeNetwork, cluster_file = fetchNetwork(prev_clustering, model, rlist, args.core_only, args.accessory_only)
-            isolateClustering = {'combined': readClusters(cluster_file, return_dict=True)}
+
+            # Use external clustering if specified
+            if args.external_clustering:
+                cluster_file = args.external_clustering
+            isolateClustering = {'combined': readClusters(cluster_file, return_dict=True, ext_clust=True)}
 
             # extract subset of distances if requested
             if args.subset is not None:
--- PopPUNK/utils.py
+++ PopPUNK/utils.py
@@ -142,7 +142,7 @@ def qcDistMat(distMat, refList, queryList, a_max):
     return passed
 
 
-def readClusters(clustCSV, return_dict=False):
+def readClusters(clustCSV, return_dict=False, ext_clust=False):
     """Read a previous reference clustering from CSV
 
     Args:
@@ -151,6 +151,8 @@ def readClusters(clustCSV, return_dict=False):
         return_type (str)
             If True, return a dict with sample->cluster instead
             of sets
+        ext_clust (bool)
+            If True, expects three columns of data rather than two
 
     Returns:
         clusters (dict)
@@ -166,7 +168,10 @@ def readClusters(clustCSV, return_dict=False):
     with open(clustCSV, 'r') as csv_file:
         header = csv_file.readline()
         for line in csv_file:
-            (sample, clust_id) = line.rstrip().split(",")
+            if ext_clust:
+                (sample, clust_id, hist) = line.rstrip().split(",")
+            else:
+                (sample, clust_id) = line.rstrip().split(",")
             if return_dict:
                 clusters[sample] = clust_id
             else:
