e1b32e1e356148dcb6cf225e8e35d348a24e0105
John Lees <lees.john6@gmail.com>
2020-11-18 12:26:22 +0000

    Add easy-run script

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
              'scripts/poppunk_calculate_silhouette.py',
              'scripts/poppunk_extract_distances.py',
              'scripts/poppunk_add_weights.py',
+             'scripts/poppunk_easy_run.py',
              'scripts/poppunk_pickle_fix.py'],
     install_requires=['numpy',
                       'scipy',
