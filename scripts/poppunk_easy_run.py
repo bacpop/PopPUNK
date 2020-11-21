ea9e4cb0cf79ac9e82bddd18eb1565f0ef5ce224
nickjcroucher <n.croucher@imperial.ac.uk>
2020-11-21 22:24:41 +0000

    Add visualisation options to easy_run script

--- scripts/poppunk_easy_run.py
+++ scripts/poppunk_easy_run.py
@@ -16,10 +16,13 @@ def get_options():
     parser.add_argument('--r-files', help='List of sequence names and files (as for --r-files')
     parser.add_argument('--output', help='Prefix for output files')
 
-    parser.add_argument('--other-args', help="Other arguments to pass to poppunk. e.g. "
+    parser.add_argument('--analysis-args', help="Other arguments to pass to poppunk. e.g. "
                                              "'--min-k 13 --max-k 29'")
+    parser.add_argument('--viz', help = "Run visualisation of output", default = False, action = "store_true")
+    parser.add_argument('--viz-args', help = "Options to use for visualisation")
     parser.add_argument('--poppunk-exe', help="Location of poppunk executable. Use "
                                               "'python poppunk-runner.py' to run from source tree")
+    parser.add_argument('--viz-exe', help = "Location of poppunk_visualisation executable")
 
     return parser.parse_args()
 
@@ -34,10 +37,10 @@ if __name__ == "__main__":
     else:
         poppunk = args.poppunk_exe
 
-    if args.other_args is None:
+    if args.analysis_args is None:
         pp_args = ""
     else:
-        pp_args = args.other_args
+        pp_args = args.analysis_args
 
     sys.stderr.write("Running --create-db\n")
     create_db_cmd = poppunk + " --create-db --r-files " + args.r_files + " --output " + args.output + " " + pp_args
@@ -53,5 +56,21 @@ if __name__ == "__main__":
     refine_cmd = poppunk + " --fit-model refine --ref-db " + args.output + " --output " + args.output + " " + pp_args
     sys.stderr.write(refine_cmd + "\n")
     subprocess.run(refine_cmd, shell=True, check=True)
+
+    if args.viz:
+
+        if args.viz_exe is None:
+            poppunk_viz = "poppunk_visualise"
+        else:
+            poppunk_viz = args.viz_exe
+
+        if args.viz_args is None:
+            viz_extra = ""
+        else:
+            viz_extra = args.viz_args
+
+        viz_cmd = poppunk_viz + " --ref-db " + args.output + " --output " + args.output + " " + viz_extra
+        sys.stderr.write(viz_cmd + "\n")
+        subprocess.run(viz_cmd, shell=True, check=True)
 
     sys.exit(0)
