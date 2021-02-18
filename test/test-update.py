2ca5dec552bccfa26169fb8b15f117aa5bac00ad
John Lees <lees.john6@gmail.com>
2021-02-18 12:47:56 +0000

    Requery in update test

--- test/test-update.py
+++ test/test-update.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
-# Copyright 2018-2020 John Lees and Nick Croucher
+# Copyright 2018-2021 John Lees and Nick Croucher
 
-"""Tests for PopPUNK"""
+"""Tests for PopPUNK --update-db order"""
 
 import subprocess
-import os
+import os, sys
 import sys
 import shutil
+import pickle
 
 import numpy as np
 from scipy import stats
+import h5py
+
+sys.path.insert(0, '/Users/jlees/Documents/Imperial/pp-sketchlib/build/lib.macosx-10.9-x86_64-3.8')
+import pp_sketchlib
 
 if os.environ.get("POPPUNK_PYTHON"):
     python_cmd = os.environ.get("POPPUNK_PYTHON")
@@ -18,8 +23,9 @@ else:
 
 def run_regression(x, y, threshold = 0.99):
     res = stats.linregress(x, y)
+    print("R^2: " + str(res.rvalue**2))
     if res.rvalue**2 < threshold:
-        sys.stderr.write("Dist order failed: R^2 = " + str(res.rvalue**2) + "\n")
+        sys.stderr.write("Distance matrix order failed!\n")
         sys.exit(1)
 
 # Check that order is the same after doing 1 + 2 with --update-db, as doing all of 1 + 2 together
@@ -28,9 +34,20 @@ subprocess.run(python_cmd + " ../poppunk-runner.py --create-db --r-files rfile1.
 subprocess.run(python_cmd + " ../poppunk-runner.py --fit-model lineage --ref-db batch1 --ranks 1", shell=True, check=True)
 subprocess.run(python_cmd + " ../poppunk_assign-runner.py --db batch1 --query rfile2.txt --output batch2 --update-db --overwrite", shell=True, check=True)
 
-X1 = np.load("batch12/batch12.dists.npy")
+# Load updated distances
 X2 = np.load("batch2/batch2.dists.npy")
+with open("batch2/batch2.dists.pkl", 'rb') as pickle_file:
+    rlist2, qlist, self = pickle.load(pickle_file)
 
+# Get same distances from the full database
+ref_db = "batch12/batch12"
+ref_h5 = h5py.File(ref_db + ".h5", 'r')
+db_kmers = sorted(ref_h5['sketches/' + rlist2[0]].attrs['kmers'])
+ref_h5.close()
+X1 = pp_sketchlib.queryDatabase(ref_db, ref_db, rlist2, rlist2, db_kmers,
+                                True, False, 1, False, 0)
+
+# Check distances match
 run_regression(X1[:, 0], X2[:, 0])
 run_regression(X1[:, 1], X2[:, 1])
 
