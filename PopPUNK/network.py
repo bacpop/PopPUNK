ff0720177fb777f5962364d5dfaed99797b5d514
nickjcroucher <n.croucher@imperial.ac.uk>
2021-03-12 16:16:50 +0000

    Change degree print statement

--- PopPUNK/network.py
+++ PopPUNK/network.py
@@ -568,7 +568,7 @@ def networkSummary(G, calc_betweenness=True, use_gpu = False):
         density = G.number_of_edges()/(0.5 * G.number_of_vertices() * G.number_of_vertices() - 1)
         triangle_count = cugraph.community.triangle_count.triangles(G)
         degree_df = G.degree()
-        print("Degree is " + str(degree))
+        print("Degree is " + str(degree_df['degree']))
         triad_count = sum([d * (d - 1) for d in degree_df['degree']])
         transitivity = triangle_count/triad_count
     else:
