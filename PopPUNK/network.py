3c7d1d25248ce3ee88008c4c90c2f22d6b927ccc
nickjcroucher <n.croucher@imperial.ac.uk>
2021-03-12 16:09:35 +0000

    Change sum of degree

--- PopPUNK/network.py
+++ PopPUNK/network.py
@@ -559,7 +559,7 @@ def networkSummary(G, calc_betweenness=True, use_gpu = False):
         density = G.number_of_edges()/(0.5 * G.number_of_vertices() * G.number_of_vertices() - 1)
         triangle_count = cugraph.community.triangle_count.triangles(G)
         degree = G.degree()
-        triad_count = sum([d * (d - 1) for d in degree)
+        triad_count = sum([d * (d - 1) for d in degree])
         transitivity = triangle_count/triad_count
     else:
         component_assignments, component_frequencies = gt.label_components(G)
