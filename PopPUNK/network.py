2e1f3eefa524036c72587b8c68cf957835548706
nickjcroucher <n.croucher@imperial.ac.uk>
2021-03-12 09:47:58 +0000

    Add edge list

--- PopPUNK/network.py
+++ PopPUNK/network.py
@@ -199,6 +199,7 @@ def extractReferences(G, dbOrder, outPrefix, existingRefs = None, threads = 1, u
         
         # Construct reference graph
         G_df = G.view_edge_list()
+        print("Edge list: " + str(G_df))
         G_ref_df = G_df[G_df['source'].isin(reference_names) & G_df['destination'].isin(reference_names)]
         G_ref = cugraph.Graph()
         G_ref.from_cudf_edgelist(G_ref_df, edge_attr='weights', renumber=False)
