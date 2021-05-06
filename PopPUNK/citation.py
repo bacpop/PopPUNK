10a66cb34c56d75c91e51be55ccb3deb4679e691
nickjcroucher <n.croucher@imperial.ac.uk>
2021-05-06 16:25:27 +0100

    Resolve strains and lineages in citation

--- PopPUNK/citation.py
+++ PopPUNK/citation.py
@@ -45,12 +45,14 @@ poppunk_methods = "We built a database of %(number_samples)s isolates using " + 
 "pp-sketchlib version %(sketchlib_version)s (doi:%(sketchlib_doi)s) with " + \
 "sketch version %(sketchlib_hash)s, k-mer lengths %(kmin)s-%(kmax)s, a " + \
 "sketch size of %(sketch_size)s and %(seed_type)s seeds [6-8]. We assigned " + \
-"variable-length-k-mer clusters (VLKCs) using PopPUNK version %(poppunk_version)s " + \
-"(doi:%(poppunk_doi)s) by fitting a %(model_mode)s with %(model_options)s [1-5].\n"
+"isolates to %(cluster_type)s through variable-length-k-mer clustering (VLKC) with PopPUNK " + \
+"version %(poppunk_version)s (doi:%(poppunk_doi)s) by fitting a %(model_mode)s " + \
+"with %(model_options)s [1-5].\n"
 
 assign_methods = "We queried a database of %(number_samples)s isolates using pp-sketchlib version" + \
 " %(sketchlib_version)s (doi:%(sketchlib_doi)s) with sketch version %(sketchlib_hash)s, " + \
-"k-mer lengths %(kmin)s-%(kmax)s, a sketch size of %(sketch_size)s and %(seed_type)s seeds [6-8]. We assigned sequences to variable-length-k-mer clusters (VLKCs) using PopPUNK " + \
+"k-mer lengths %(kmin)s-%(kmax)s, a sketch size of %(sketch_size)s and %(seed_type)s seeds [6-8]. We " + \
+"sequence were classified using variable-length-k-mer clustering (VLKC) with PopPUNK " + \
 "version %(poppunk_version)s (doi:%(poppunk_doi)s) [1-5].\n"
 
 def print_citation(args, assign=False):
@@ -92,27 +94,34 @@ def print_citation(args, assign=False):
                                  "poppunk_version" : __version__,
                                  "poppunk_doi" : "10.1101/gr.241455.118"})
     else:
+
         if args.fit_model == "bgmm":
             model_mode = "BGMM"
             model_options = str(args.K) + " components"
+            cluster_type = "strains"
         elif args.fit_model == "dbscan":
             model_mode = "HDBSCAN"
             model_options = str(args.D) + " maximum clusters and a minimum of "  + \
                     str(round((n_samples * (n_samples + 1))/(2 * args.min_cluster_prop))) + \
                     " points per cluster"
+            cluster_type = "strains"
         elif args.fit_model == "refine":
             model_mode = "refined"
             model_options = "score " + str(args.score_idx) + " used to optimise the VLKCs"
+            cluster_type = "strains"
         elif args.fit_model == "threshold":
             model_mode = "simple threshold"
             model_options = "a core-distance cutoff distance of " + args.threshold
+            cluster_type = "partitions"
         elif args.fit_model == "lineage":
             model_mode = "lineage"
             model_options = "ranks of " + args.ranks
+            cluster_type = "lineages"
         else:
             model_mode = "UNKNOWN"
             model_options = "UNKNOWN"
-
+            cluster_type = "UNKNOWN"
+    
         print(poppunk_methods % {"number_samples" : n_samples,
                                  "sketchlib_version" : pp_sketchlib_version,
                                  "sketchlib_doi" : "10.5281/zenodo.4531418",
@@ -121,6 +130,7 @@ def print_citation(args, assign=False):
                                  "kmax" : kmax,
                                  "sketch_size" : sketch_size,
                                  "seed_type" : seed_phasing,
+                                 "cluster_type": cluster_type,
                                  "poppunk_version" : __version__,
                                  "poppunk_doi" : "10.1101/gr.241455.118",
                                  "model_mode" : model_mode,
