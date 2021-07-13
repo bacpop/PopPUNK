105db6cf4ca6c9b7c80f62ba5c2e8699897c2c3a
nickjcroucher <n.croucher@imperial.ac.uk>
2021-07-13 14:49:27 +0100

    Add script for comparing network properties and sequence properties

---
+++ scripts/poppunk_sample_info.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# vim: set fileencoding=<utf-8> :
+# Copyright 2018-2020 John Lees and Nick Croucher
+
+import sys
+import argparse
+import h5py
+import pandas as pd
+from scipy import sparse
+
+# Load GPU libraries
+try:
+    import cupyx
+    import cugraph
+    import cudf
+    import cupy as cp
+    from numba import cuda
+    import rmm
+    gpu_lib = True
+except ImportError as e:
+    gpu_lib = False
+
+def setGtThreads(threads):
+    import graph_tool.all as gt
+    # Check on parallelisation of graph-tools
+    if gt.openmp_enabled():
+        gt.openmp_set_num_threads(threads)
+        sys.stderr.write('\nGraph-tools OpenMP parallelisation enabled:')
+        sys.stderr.write(' with ' + str(gt.openmp_get_num_threads()) + ' threads\n')
+
+def load_network_file(fn, use_gpu = False):
+    """Load the network based on input options
+
+       Returns the network as a graph-tool format graph, and sets
+       the slope parameter of the passed model object.
+
+       Args:
+            fn (str)
+                Network file name
+            use_gpu (bool)
+                Use cugraph library to load graph
+
+       Returns:
+            genomeNetwork (graph)
+                The loaded network
+    """
+    # Load the network from the specified file
+    if use_gpu:
+        G_df = cudf.read_csv(fn, compression = 'gzip')
+        genomeNetwork = cugraph.Graph()
+        if 'weights' in G_df.columns:
+            G_df.columns = ['source','destination','weights']
+            genomeNetwork.from_cudf_edgelist(G_df, edge_attr='weights', renumber=False)
+        else:
+            G_df.columns = ['source','destination']
+            genomeNetwork.from_cudf_edgelist(G_df,renumber=False)
+        sys.stderr.write("Network loaded: " + str(genomeNetwork.number_of_vertices()) + " samples\n")
+    else:
+        genomeNetwork = gt.load_graph(fn)
+        sys.stderr.write("Network loaded: " + str(len(list(genomeNetwork.vertices()))) + " samples\n")
+
+    return genomeNetwork
+
+def check_and_set_gpu(use_gpu, gpu_lib, quit_on_fail = False):
+    """Check GPU libraries can be loaded and set managed memory.
+
+    Args:
+        use_gpu (bool)
+            Whether GPU packages have been requested
+        gpu_lib (bool)
+            Whether GPU packages are available
+    Returns:
+        use_gpu (bool)
+            Whether GPU packages can be used
+    """
+    # load CUDA libraries
+    if use_gpu and not gpu_lib:
+        if quit_on_fail:
+            sys.stderr.write('Unable to load GPU libraries; exiting\n')
+            sys.exit(1)
+        else:
+            sys.stderr.write('Unable to load GPU libraries; using CPU libraries '
+            'instead\n')
+            use_gpu = False
+
+    # Set memory management for large networks
+    if use_gpu:
+        rmm.reinitialize(managed_memory=True)
+        cudf.set_allocator("managed")
+        if "cupy" in sys.modules:
+            cupy.cuda.set_allocator(rmm.rmm_cupy_allocator)
+        if "cuda" in sys.modules:
+            cuda.set_memory_manager(rmm.RMMNumbaManager)
+        assert(rmm.is_initialized())
+
+    return use_gpu
+
+def sparse_mat_to_network(sparse_mat, rlist, use_gpu = False):
+    """Generate a network from a lineage rank fit
+
+    Args:
+       sparse_mat (scipy or cupyx sparse matrix)
+         Sparse matrix of kNN from lineage fit
+       rlist (list)
+         List of sequence names
+       use_gpu (bool)
+         Whether GPU libraries should be used
+
+    Returns:
+      G (network)
+          Graph tool or cugraph network
+    """
+    if use_gpu:
+        G_df = cudf.DataFrame(columns = ['source','destination','weights'])
+        G_df['source'] = sparse_mat.row
+        G_df['destination'] = sparse_mat.col
+        G_df['weights'] = sparse_mat.data
+        max_in_vertex_labels = len(rlist)-1
+        G = add_self_loop(G_df, max_in_vertex_labels, weights = True, renumber = False)
+    else:
+        connections = []
+        for (src,dst) in zip(sparse_mat.row,sparse_mat.col):
+            connections.append(src,dst)
+        G = construct_network_from_edge_list(rlist,
+                                               rlist,
+                                               connections,
+                                               weights = sparse_mat.data,
+                                               summarise=False)
+
+    return G
+
+# command line parsing
+def get_options():
+
+    parser = argparse.ArgumentParser(description='Get information about a PopPUNK database',
+                                     prog='poppunk_db_info')
+
+    # input options
+    parser.add_argument('--db',
+                        required = True,
+                        help='Database file (.h5)')
+    parser.add_argument('--network',
+                        required = True,
+                        help='Network or lineage fit file for analysis')
+    parser.add_argument('--threads',
+                        default = 1,
+                        help='Number of cores to use in analysis')
+    parser.add_argument('--use-gpu',
+                        default = False,
+                        action = 'store_true',
+                        help='Whether GPU libraries should be used in analysis')
+    parser.add_argument('--output',
+                        required = True,
+                        help='Prefix for output files')
+
+    return parser.parse_args()
+
+# main code
+if __name__ == "__main__":
+
+    # Check input ok
+    args = get_options()
+
+    # Check whether GPU libraries can be loaded
+    use_gpu = check_and_set_gpu(use_gpu, gpu_lib, quit_on_fail = False)
+
+    # Set threads for graph-tool
+    setGtThreads(args.threads)
+
+    # Open and process sequence database
+    ref_db = h5py.File(args.db, 'r')
+    sample_names = list(ref_db['sketches'].keys())
+    
+    sample_sequence_length = {}
+    sample_missing_bases = {}
+    sample_base_frequencies = {name: [] for name in sample_names}
+    
+    for sample_name in sample_names:
+        sample_base_frequencies[sample_name] = ref_db['sketches/' + sample_name].attrs['base_freq']
+        sample_sequence_length[sample_name] = ref_db['sketches/' + sample_name].attrs['length']
+        sample_missing_bases[sample_name] = ref_db['sketches/' + sample_name].attrs['missing_bases']
+        
+    # Open network file
+    if args.network.endswith('.gt'):
+        G = load_network_file(args.network, use_gpu = False)
+    elif args.network.endswith('.csv.gz'):
+        if use_gpu:
+            G = load_network_file(args.network, use_gpu = True)
+        else:
+            sys.stderr.write('Unable to load necessary GPU libraries\n')
+            exit(1)
+    elif args.network.endswith('.npz'):
+        sparse_mat = sparse.load_npz(args.network)
+        G = sparse_mat_to_network(sparse_mat, sample_names, use_gpu = use_gpu)
+    else:
+        sys.stderr.write('Unrecognised suffix: expected ".gt", ".csv.gz" or ".npz"\n')
+        exit(1)
+
+    # Analyse network
+    if use_gpu:
+        component_assignments_df = cugraph.components.connectivity.connected_components(G)
+        component_assignments_df['component_count'] = component_assignments.groupby('partition')['vertex'].transform('count')
+        outdegree_df = cugraph.out_degree(G)
+        graph_properties_df = component_assignments_df.merge(outdegree_df, on = ['vertex'])
+    else:
+        graph_properties_df = pd.DataFrame()
+        graph_properties_df['vertex'] = np.arange(len(rlist))
+        graph_properties_df['partition'] = gt.label_components(G)[0].a
+        graph_properties_df['outdegree'] = G.get_out_degrees(G.get_vertices())
+        graph_properties_df['component_count'] = component_assignments.groupby('partition')['vertex'].transform('count')
+    graph_properties_df['vertex'] = sample_names
+    
+    # Merge data and print output
+    with open(args.output,'w') as out_file:
+        out_file.write('Sample,Length,Missing_bases,Frequency_A,Frequency_C,Frequency_G,Frequency_T,Component,Component_size,Node_degree\n')
+        for i,sample_name in enumerate(sample_names):
+            out_file.write(sample_name + ',' + str(sample_sequence_length[sample_name]) + ',' + str(sample_missing_bases[sample_name]) + ','
+            for frequency in sample_base_frequencies[sample_name]:
+                out_file.write(str(frequency) + ','
+            out_file.write(str(graph_properties_df['partition'].where(graph_properties_df['vertex']==sample_name)))
+            out_file.write(str(graph_properties_df['partition'].where(graph_properties_df['vertex']==sample_name)))
+            out_file.write(str(graph_properties_df['outdegree'].where(graph_properties_df['vertex']==sample_name)))
+            out_file.write("\n")
+
+    sys.exit(0)
