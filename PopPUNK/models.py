f721ca1edffb3012e71d480a5fbca4c2a2378db3
Nick Croucher <n.croucher@imperial.ac.uk>
2024-02-23 11:12:21 +0000

    Clarify awful min_sample condition

--- PopPUNK/models.py
+++ PopPUNK/models.py
@@ -513,7 +513,8 @@ class DBSCANFit(ClusterFit):
 
         # DBSCAN parameters
         cache_out = "./" + self.outPrefix + "_cache"
-        min_samples = min(int(min_cluster_prop * self.subsampled_X.shape[0]), 1023)
+        min_samples = max(int(min_cluster_prop * self.subsampled_X.shape[0]), 10) # do not allow clusters of < 10 points
+        min_samples = min(min_samples,1023) # do not allow clusters to require more than 1023 points at the start
         min_cluster_size = max(int(0.01 * self.subsampled_X.shape[0]), 10)
 
         # Check on initialisation of GPU libraries and memory
