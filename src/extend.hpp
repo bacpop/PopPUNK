27e9731daf0b541cab840b78466c1d4d01496900
Nick Croucher <n.croucher@imperial.ac.uk>
2025-07-21 14:08:34 +0100

    Fix python binding

--- src/extend.hpp
+++ src/extend.hpp
@@ -17,7 +17,7 @@ sparse_coo lower_rank(const sparse_coo &sparse_rr_mat,
                       bool reciprocal_only,
                       bool count_unique_distances,
                       float epsilon,
-                      size_t num_threads = 1);
+                      const size_t num_threads);
 
 sparse_coo get_kNN_distances(const NumpyMatrix &distMat,
                              const int kNN,
